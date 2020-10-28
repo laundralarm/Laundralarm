3cf95ba214188cfbcaefcd07afd15cc911221145
kootsoop <peter@kootsoop.com>
2020-10-28 19:23:04 -0400

    Added some PyCharm extras. Made some changes to let PyCharm be happy with formatting. Some suggested changes to make the code a bit more secure.

--- text.py
+++ text.py
@@ -1,23 +1,25 @@
 import smtplib
+import os
 from email.message import EmailMessage
 
+
 def email_alert(subject, body, to):
-	msg = EmailMessage()
-	msg.set_content(body)
-	msg['subject'] = subject
-	msg['to'] = to
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['subject'] = subject
+    msg['to'] = to
 
-	user = "laundralarmalert@gmail.com"
-	msg['from'] = user
-	password = "nqyyglqcbyqeaohq"
+    user = os.environ['LAUNDRALARM_USER']
+    msg['from'] = user
+    password = os.environ['LAUNDRALARM_PASSWORD']
 
-	server = smtplib.SMTP("smtp.gmail.com", 587)
-	server.starttls()
-	server.login(user, password)
-	server.send_message(msg)
-	
-	server.quit()
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+
+    server.quit()
 
 
-if _name_ == '__main__':
-	email_alert("Hey", "Hello world", "3478605680@vtext.com")
+if __name__ == '__main__':
+    email_alert("Hey", "Hello world", "3478605680@vtext.com")
--- time.py
+++ time.py
@@ -1,19 +1,17 @@
-
 import time
 
-#User Input 
-type = input("Are you using your Washer or Dryer? ")
-#User phone number
-phonenumber = input("Where would you like to receive texts? ")
-#Printing out confirmation
-print("You selected: " + type + "and your phone number is" + phonenumber)
+# User Input
+typeOfDevice = input("Are you using your Washer or Dryer? ")
+# User phone number
+phoneNumber = input("Where would you like to receive texts? ")
+# Printing out confirmation
+print("You selected: " + typeOfDevice + "and your phone number is" + phoneNumber)
 
-if type == "washer":
-    time.sleep(5) #wait 5 seconds
-    #will add code here
+if typeOfDevice == "washer":
+    time.sleep(5)  # wait 5 seconds
+    # will add code here
     print("Timer is Done")
-elif type == "dryer":
-    time.sleep(10) #wait 10 seconds
-    #will add code here
+elif typeOfDevice == "dryer":
+    time.sleep(10)  # wait 10 seconds
+    # will add code here
     print("Timer is Done")
-
