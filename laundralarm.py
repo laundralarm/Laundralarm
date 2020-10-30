c41f734e560f3542ef970c2b7c70689359878b74
laundralarm <73048107+laundralarm@users.noreply.github.com>
2020-10-29 20:19:53 -0400

    Update laundralarm.py

--- laundralarm.py
+++ laundralarm.py
@@ -5,11 +5,10 @@ channel = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 def callback(channel):
-        if GPIO.input(channel):
-                print "Movement Detected!"
-        else:
-                print "Movement Detected!"
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+     GPIO.input(channel):
+         print "Movement Detected!"
+      
+GPIO.add_event_detect(channel, GPIO.RISING, bouncetime=100)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
 # infinite loop
 while True:
