6816c361e219383c97159a3ab341a21b4cd1e245
kootsoop <peter@kootsoop.com>
2020-11-02 12:57:21 -0500

    Showing how to store timestamp.

--- laundralarm.py
+++ laundralarm.py
@@ -7,8 +7,11 @@ channel = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
+last_callback_time = time.time()
+
 
 def callback(channelIn):
+    last_callback_time = time.time()
     print("Movement Detected!")
 
 
@@ -17,4 +20,6 @@ GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when th
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
 # infinite loop
 while True:
+    if time.time() - last_callback_time  > 300:
+        # send notificaiton
     time.sleep(1)
