77b7f9399869b381e7e500fb4674c59191df7d91
laundralarm <73048107+laundralarm@users.noreply.github.com>
2020-11-12 15:43:10 -0500

    Update laundralarm.py

--- laundralarm.py
+++ laundralarm.py
@@ -1,24 +1,17 @@
 import RPi.GPIO as GPIO
 import time
-from notify_run import Notify
-notify = Notify()
+import os
+from twilio.rest import Client
 
 
 print("Welcome to Laundralarm!")
 time.sleep(2)
-print("You will now be prompted a QR code to receive texts")
-time.sleep(2)
 
-#answer = input("Would you like to proceed? yes/no ")
+account_sid = 'AC789b7a1c670bad86371d94606b55c77a'
+auth_token = '8cb62de772776e509246f67ee09c9da9'
+client = Client(account_sid, auth_token)
 
-#if answer == yes:
-   # print("Please scan the QR code and tap on the subscribe button to receive an aler when your laundry is done")
-#else:
-    #quit()
-    
-time.sleep(2)
-print(notify.register()) #Prints QR Code to register channel
-time.sleep(10)
+
 print("System is starting.....")
 #GPIO SETUP
 channel = 17
@@ -39,7 +32,12 @@ GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run f
 # while loop    
 while True:
     if time.time() - last_callback_time > 20: #If no vibration is detected for 10 sec, print CYCLE FINISHED
-        notify.send('Your cycle is done!') #Send notification to user 
+        message = client.messages \
+                .create(
+                     body="Your cycle is done!",
+                     from_='+13345083288',
+                     to='+13478605680'
+                 )#Send notification to user 
         break 
         time.sleep(1)
         
