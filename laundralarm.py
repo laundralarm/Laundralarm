65d285eb9d9d3cf4a6300c3b66d7639534270527
laundralarm <73048107+laundralarm@users.noreply.github.com>
2020-11-15 21:20:07 -0500

    Update laundralarm.py

--- laundralarm.py
+++ laundralarm.py
@@ -4,11 +4,13 @@ import os
 from twilio.rest import Client
 
 
-print("Welcome to Laundralarm!")
+print("Welcome to Laundralarm! We make your life easier by notifying you when your laundry cycle is done.")
 time.sleep(2)
+number = input("Before we proceed, please provide your phone number: ")
 
-account_sid = 'AC789b7a1c670bad86371d94606b55c77a'
-auth_token = '8cb62de772776e509246f67ee09c9da9'
+
+account_sid = '' #left blank for privacy
+auth_token = '' #left blank for privacy
 client = Client(account_sid, auth_token)
 
 
@@ -19,11 +21,14 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
 last_callback_time = time.time()
+callback_called = False
 
 
 def callback(channel):
+        callback_called = True
         last_callback_time = time.time()
-        print ("Machine is running")
+        print ("Machine is running" )
+        print(last_callback_time)
        
 
 GPIO.add_event_detect(channel, GPIO.RISING, bouncetime=50)  # let us know when the pin goes HIGH or LOW
@@ -31,15 +36,16 @@ GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run f
 
 # while loop    
 while True:
-    if time.time() - last_callback_time > 20: #If no vibration is detected for 10 sec, print CYCLE FINISHED
+    if time.time() - last_callback_time > 5 and callback_called == True:#If no vibration is detected for 5 sec, print CYCLE FINISHED
+        callback_called = False 
         message = client.messages \
                 .create(
                      body="Your cycle is done!",
                      from_='+13345083288',
-                     to='+13478605680'
+                     to= number
                  )#Send notification to user 
         break 
-        time.sleep(1)
+        exit()
         
         
         
