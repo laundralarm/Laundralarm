6508294a7bafa3bec82dc0500c1d415cf1b6b0bd
kootsoop <peter@kootsoop.com>
2020-11-16 08:54:18 -0500

    Minor update to not use magic number.

--- example_pjk.py
+++ example_pjk.py
@@ -6,6 +6,7 @@ import time
 last_callback_time = time.time()  # Initialize the last time we had a vibration.
 waiting_time = time.time()  # Initialize the timer for waiting.
 seconds_to_count_down = 3   # How long we want to wait before our five second countdown.
+no_vibration_wait_time = 5  # How long until after the vibration stops that we send the message.
 keep_looping = True         # Use a boolean for the while condition.
 
 
@@ -19,10 +20,12 @@ while keep_looping:
     if time.time() - waiting_time < seconds_to_count_down:
         last_callback_time = callback(17)
         print(time.time() - last_callback_time)
+    else:
+        print("Now we wait for " + str(no_vibration_wait_time - (time.time() - last_callback_time)) + " seconds.")
     # Once last_callback_time is no longer being updated,
     # and we've waited for five seconds, this will print
     # the message and stop the loop.
-    if time.time() - last_callback_time > 5:
+    if time.time() - last_callback_time > no_vibration_wait_time:
         print("No input for several seconds!")
         keep_looping = False
 
